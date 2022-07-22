c54d38d980cccb588fb85576ad1703592a159ced
muhammetikbalbayram <muhammetikbalbayram@gmail.com>
2022-07-22 17:20:07 +0300

    add features on BlogPost component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,17 @@
 <template>
   <div class="home">
+    <blog-post :post="welcomeScreen"></blog-post>
+    <blog-post :post="post" v-for="(post,index) in sampleBlogPost" :key="index"></blog-post>
   </div>
 </template>
 
 <script>
+import BlogPost from "@/components/BlogPost";
 export default {
   name: "Home",
-  components: {},
+  components: {
+    BlogPost
+  },
   data(){
     return{
       welcomeScreen: {
